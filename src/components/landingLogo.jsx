1f075b2a0c85424bbb3707bb105287adeb27f018
Dawid Mszanowski <dmszanowski@icloud.com>
2020-10-10 23:10:51 +0200

    Fix images to work on gh-pages and localhost

--- src/components/landingLogo.jsx
+++ src/components/landingLogo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const LandingLogo = () => {
-    let imgLogoSource = '/img/logos/aestral_logo_slim.png';
+    let imgLogoSource = './img/logos/aestral_logo_slim.png';
     return (
         <div>
             <img width="100%" src={imgLogoSource} alt=""/>
--- src/components/social.jsx
+++ src/components/social.jsx
@@ -4,10 +4,10 @@ import classes from './social.module.css'
 export const Social = () => {
 
     let socials = [
-        {href:'https://facebook.com/aestralmusic', img:'/img/social/fb.png'},
-        {href:'https://instagram.com/aestraldepth', img:'/img/social/ig.png'},
-        {href:'https://soundcloud.com/aestral', img:'/img/social/spotify.png'},
-        {href:'https://open.spotify.com/artist/0WH6jkqBDDzQYQx3jSuNgL?si=4-dVD0eAQC6-2K5Xeu-s9w', img:'/img/social/soundcloud.png'},
+        {href:'https://facebook.com/aestralmusic', img:'./img/social/fb.png'},
+        {href:'https://instagram.com/aestraldepth', img:'./img/social/ig.png'},
+        {href:'https://soundcloud.com/aestral', img:'./img/social/spotify.png'},
+        {href:'https://open.spotify.com/artist/0WH6jkqBDDzQYQx3jSuNgL?si=4-dVD0eAQC6-2K5Xeu-s9w', img:'./img/social/soundcloud.png'},
     ]
     return (
         <div className={classes.socialWrapper}>
