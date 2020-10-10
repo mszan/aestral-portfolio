fdb7d3247ef9ddd72d30a512989190799610ab89
Dawid Mszanowski <dmszanowski@icloud.com>
2020-10-10 22:53:21 +0200

    Updates

---
+++ precache-manifest.dfaf27e36910869a8e234f35bd9523f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181fe9b1dbf597a4a09fc7b49cc0ca18",
+    "url": "/aestral_portfolio/index.html"
+  },
+  {
+    "revision": "1e2dd3ef53afe432222e",
+    "url": "/aestral_portfolio/static/css/main.d681d992.chunk.css"
+  },
+  {
+    "revision": "2ce3cc639ab82b98e196",
+    "url": "/aestral_portfolio/static/js/2.1cdf021c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aestral_portfolio/static/js/2.1cdf021c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2dd3ef53afe432222e",
+    "url": "/aestral_portfolio/static/js/main.6ba0c430.chunk.js"
+  },
+  {
+    "revision": "893bd6744b96190aaccf",
+    "url": "/aestral_portfolio/static/js/runtime-main.8acd9cd9.js"
+  }
+]);
